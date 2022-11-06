a5f43920bc34fa197f08746388e8af5f6b8aab73
reginasam2509 <65501837+reginasam2509@users.noreply.github.com>
2022-11-06 12:50:29 +0530

    Add files via upload
    Week1_Assignments

---
+++ Week1_Assign_1/Mobile.java
@@ -0,0 +1,24 @@
+package Week1_Assign_1;
+
+public class Mobile {
+	String mobileBrandName="Samsung";
+	char mobileLogo='S';
+	short noOfMobilePiece=1;
+	int modelNumber=234543;
+	long mobilemeiNumber=46546457;
+	float mobilePrice=24000.00f;
+	boolean isDamaged=false;
+	public static void main(String[] args)
+	{
+		Mobile mb= new Mobile();
+		System.out.println("mb.mobileBrandName");
+		System.out.println("mobileLogo");
+		System.out.println("noOfMobilePiece");
+		System.out.println("modelNumber");
+		System.out.println("mobilemeiNumber");
+		System.out.println("mobilePrice");
+		System.out.println("isDamaged");
+		
+	}
+
+}
---
+++ Week1_Assign_1/TwoWheeler.java
@@ -0,0 +1,24 @@
+package Week1_Assign_1;
+
+public class TwoWheeler {
+	int noOfWheels=2;
+	short noOfMirrors=2;
+	long chassisnumber=123454546;
+	boolean isPunctured=false;
+	String bikeName="Himalayan";
+	double  runningKM=4345.34;
+	
+	public static void main(String[] args) {
+		 TwoWheeler Tw=new TwoWheeler();
+		 System.out.println(Tw.noOfWheels);
+	     System.out.println(Tw.noOfMirrors);
+	     System.out.println(Tw.chassisnumber);
+	     System.out.println(Tw.isPunctured);
+	     System.out.println(Tw.bikeName);
+	     System.out.println(Tw.runningKM);
+	     
+	     
+	}
+	
+
+}
